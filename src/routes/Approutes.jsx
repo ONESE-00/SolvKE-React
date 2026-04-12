3cef0515170b330f0e721a32147db93736cb9ca8
Onese Gachogu <ogachogu@emtechhouse.co.ke>
2026-04-12 13:09:54 +0300

    auth routing

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -7,15 +7,21 @@ import SolvBankingLayout from "@/components/solv-banking/Solv-Banking-Layout";
 import Dashboard  from "@/components/solv-banking/Dashboard";
 import AccountDetails from "@/components/solv-banking/Account-Details";
 import MyProfile from "@/components/solv-banking/My-Profile";
+import AuthLayout from "@/components/authentication/Auth-Layout";
 export default function AppRoutes() {
     return (
         <Routes>
 
             {/* Authentication Routes */}
             <Route path='' element={<Login />} />
-            <Route path='/auth/signup' element={<Signup />} />
-            <Route path='/auth/forgot-password' element={<ForgotPassword />} />
-            <Route path='/auth/reset-password' element={<ResetPassword />} />
+            <Route path='/auth' element={<AuthLayout />}>
+                <Route index element={<Login />} />
+                <Route path='signup' element={<Signup />} />
+                <Route path='forgot-password' element={<ForgotPassword />} />
+                <Route path='reset-password' element={<ResetPassword />} />
+                <Route path='*' element={<Login />} />
+            </Route>
+
 
             {/* Solv Banking Routes */}
             <Route path='/solv-banking/*' element={<SolvBankingLayout />}>
